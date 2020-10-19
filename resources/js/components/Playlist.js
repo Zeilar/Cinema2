d30cae7e6172939625acf807cac0b9e98fa32ca7
Philip <philip_angelin@hotmail.com>
2020-10-19 10:48:46 +0200

    Revert "Changed camel case to kebab"
    This reverts commit 327fb9f2941dfabcb071ba143b28d9f4cc147c7f.

--- resources/js/components/Playlist.js
+++ resources/js/components/Playlist.js
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 export default function Playlist() {
     return (
         <div>
-            PLaylist
+            
         </div>
     );
 }
